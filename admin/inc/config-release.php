6e477b3f74fd08ef7dbe57bf1d6279afdad61c0f
intermedia <intermediacolombia@gmail.com>
2026-07-23 11:56:16 -0500

    Remove config files from tracking and update .gitignore

--- admin/inc/config-release.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "soluctem_sistema";
-$password = "J,*}1Wr6]9~a";
-$dbname = "soluctem_sistema_release";
-$url_sys = "https://sistema.soluctem.com.co"
-?>
